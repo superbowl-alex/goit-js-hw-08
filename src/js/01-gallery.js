49b4c69ed61268316b4006a7517ad4529b9521d4
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-08-01 21:27:50 +0300

    Task01, Task02 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+lightbox.on('show.lightbox');
+
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
 
+const iframeRef = document.querySelector('iframe');
+const player = new Player(iframeRef);
+const throttle = require('lodash.throttle');
+const CURRENT_TIME = 'videoplayer-current-time';
+
+let savedTime = localStorage.getItem(CURRENT_TIME);
+let storageTime = checkStorageTime(savedTime);
+
+player.setCurrentTime(storageTime).then(function (seconds) {});
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function checkStorageTime(time) {
+  return time ? JSON.parse(time) : 0;
+}
+
+function saveCurrentTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+  });
+}
