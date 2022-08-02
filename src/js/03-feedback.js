c6716b309ac489a15ad3f7fbd38f5c8de4da7bae
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-08-02 16:17:29 +0300

    Task03 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+const throttle = require('lodash.throttle');
 
+const formRef = document.querySelector('.feedback-form');
+
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+const formData = {};
+
+formRef.addEventListener('input', throttle(inputFormFields, 500));
+formRef.addEventListener('submit', submitForms);
+
+updateFormFields();
+
+function inputFormFields(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
+}
+
+function submitForms(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_FORM_STATE);
+}
+
+function updateFormFields() {
+  try {
+    const savedData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
+    if (savedData) {
+      formRef.email.value = savedData.email;
+      formRef.message.value = savedData.message;
+    }
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+  }
+}
