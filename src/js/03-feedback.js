7a4271ded895a72806ead6c36396587ee27533ab
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-08-04 17:13:42 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,57 @@
 const throttle = require('lodash.throttle');
 
 const formRef = document.querySelector('.feedback-form');
-
 const FEEDBACK_FORM_STATE = 'feedback-form-state';
-let savedStorageData;
+
 formRef.addEventListener('input', throttle(inputFormFields, 500));
 formRef.addEventListener('submit', submitForms);
-document.addEventListener('DOMContentLoaded', updateFormFields);
+addEventListener('DOMContentLoaded', updateFormFields);
 
 function inputFormFields(e) {
-  savedStorageData[e.target.name] = e.target.value;
-  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(savedStorageData));
+  const storageData = loadData(FEEDBACK_FORM_STATE, {});
+  storageData[e.target.name] = e.target.value;
+  saveData(FEEDBACK_FORM_STATE, storageData);
 }
 
 function submitForms(e) {
   e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem(FEEDBACK_FORM_STATE);
-  console.log(savedStorageData);
+  validateForm(e);
+  const submitdData = loadData(FEEDBACK_FORM_STATE, {});
+  if (Object.keys(submitdData).length === 2) {
+    e.currentTarget.reset();
+    console.log(submitdData);
+    localStorage.removeItem(FEEDBACK_FORM_STATE);
+  }
 }
 
 function updateFormFields() {
+  const updateData = loadData(FEEDBACK_FORM_STATE, {});
+  formRef.email.value = updateData.email || '';
+  formRef.message.value = updateData.message || '';
+}
+
+function saveData(key, value) {
   try {
-    const savedData = localStorage.getItem(FEEDBACK_FORM_STATE);
-    savedStorageData = savedData ? JSON.parse(savedData) : {};
-    if (savedStorageData) {
-      formRef.email.value = savedStorageData.email || '';
-      formRef.message.value = savedStorageData.message || '';
-    }
+    localStorage.setItem(key, JSON.stringify(value));
   } catch (error) {
-    console.log(error.name);
-    console.log(error.message);
+    console.error('Set state error: ', error.message);
   }
+}
+
+function loadData(key, defaultValue) {
+  try {
+    const result = JSON.parse(localStorage.getItem(key));
+    return result ? result : defaultValue;
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+}
+
+function validateForm(e) {
+  const formData = new FormData(e.currentTarget);
+  formData.forEach((value, name) => {
+    if (value === '') {
+      return alert('Please fill in all the fields!');
+    }
+  });
 }
